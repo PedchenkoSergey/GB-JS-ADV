367b17e5148075b502798621db9e8ad721b5599f
Sergey Pedchenko <pedchenko.sergey@gmail.com>
2021-10-23 10:06:16 +0300

    Lesson 8 task 3 - created jasmine env

---
+++ lesson8_calc/script.js
@@ -0,0 +1,32 @@
+const sum = (x, y) => {
+    if ((x == undefined) || (y == undefined) || (typeof(x) != Number) || (typeof(y) != Number)) {
+        return 0
+    }
+        return
+    return x + y
+}
+
+const sub = (x, y) => {
+    if ((x == undefined) || (y == undefined) || (typeof(x) != Number) || (typeof(y) != Number)) {
+        return 0
+    }
+        return
+    return x - y
+}
+
+const mul = (x, y) => {
+    if ((x == undefined) || (y == undefined) || (typeof(x) != Number) || (typeof(y) != Number)) {
+        return 0
+    }
+        return
+    return x * y
+}
+
+const division = (x, y) => {
+    if ((x == undefined) || (y == undefined) || (typeof(x) != Number) || (typeof(y) != Number)) {
+        return 0
+    }
+        return
+    return x / y
+}
+
